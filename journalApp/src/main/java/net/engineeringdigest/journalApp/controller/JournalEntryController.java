d26a96502e7df71d01d937cd456f5f245e366db8
Aruj Anand <arujanand@protonmail.com>
2025-07-21 18:46:15 +0530

    MVC architecture incroporated with DB connection

--- journalApp/src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
@@ -1,42 +1,46 @@
 package net.engineeringdigest.journalApp.controller;
 
 import net.engineeringdigest.journalApp.entity.JournalEntry;
+import net.engineeringdigest.journalApp.service.JournalEntryService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/journal")
 public class JournalEntryController {
 
-    private final Map<Long, JournalEntry> journalEntries = new HashMap<>();
+    @Autowired
+    private JournalEntryService journalEntryService;
 
     @GetMapping("/all-journals")
     public List<JournalEntry> getAll() {
-        return new ArrayList<>(journalEntries.values());
+        return journalEntryService.getAll();
     }
 
     @PostMapping
-    public boolean createEntry(@RequestBody JournalEntry Entry) {
-        journalEntries.put(Entry.getId(), Entry);
-        return true;
+    public JournalEntry createEntry(@RequestBody JournalEntry Entry) {
+        Entry.setDate(LocalDateTime.now());
+        journalEntryService.saveEntry(Entry);
+        return Entry;
     }
 
     @GetMapping("/id/{myId}")
-    public JournalEntry getJournalById(@PathVariable long myId) {
-        return journalEntries.get(myId);
+    public JournalEntry getJournalById(@PathVariable ObjectId myId) {
+        return journalEntryService.findById(myId).orElse(null);
     }
 
     @DeleteMapping("id/{myId}")
-    public boolean deleteById(@PathVariable long myId) {
-        return journalEntries.remove(myId) != null;
+    public boolean deleteById(@PathVariable ObjectId myId) {
+        journalEntryService.deleteById(myId);
+        return true;
     }
 
     @PutMapping("/id/{myId}")
-    public JournalEntry updateJournalById(@PathVariable Long myId, @RequestBody JournalEntry myEntry) {
-        return journalEntries.put(myId,myEntry);
+    public JournalEntry updateJournalById(@PathVariable ObjectId myId, @RequestBody JournalEntry myEntry) {
+        return journalEntryService.updateById(myId, myEntry);
     }
 }
---
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
@@ -0,0 +1,45 @@
+package net.engineeringdigest.journalApp.service;
+
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import net.engineeringdigest.journalApp.repository.JournalEntryRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class JournalEntryService {
+
+    @Autowired
+    private JournalEntryRepository journalEntryRepository;
+
+    public void saveEntry(JournalEntry journalEntry) {
+        journalEntryRepository.save(journalEntry);
+    }
+
+    public List<JournalEntry> getAll() {
+        return journalEntryRepository.findAll();
+    }
+
+    public Optional<JournalEntry> findById(ObjectId myId) {
+        return journalEntryRepository.findById(myId);
+    }
+
+    public void deleteById (ObjectId myId) {
+        journalEntryRepository.deleteById(myId);
+    }
+
+    public JournalEntry updateById(ObjectId myId, JournalEntry newEntry) {
+        JournalEntry oldEntry = journalEntryRepository.findById(myId).orElse(null);
+
+        if (oldEntry != null) {
+            oldEntry.setTitle(newEntry.getTitle() !=  null && !newEntry.getTitle().isEmpty() ? newEntry.getTitle() : oldEntry.getTitle());
+            oldEntry.setContent(newEntry.getContent() != null && !newEntry.getContent().isEmpty() ? newEntry.getContent() : oldEntry.getContent());
+            journalEntryRepository.save(oldEntry);
+            return oldEntry;
+        }
+        return null;
+    }
+}
